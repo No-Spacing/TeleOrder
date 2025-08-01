a9a8c914086eed58c40dfd29725fd3f8eea7d009
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-08-01 15:32:03 +0800

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,4 +9,9 @@ class Role extends Model
     protected $fillable = [
         'role',
     ];
+
+    public const IS_APPROVER = 1;
+    public const IS_ENCODER = 2;
+    public const IS_SALES = 3;
+    public const IS_ADMIN = 4;
 }
