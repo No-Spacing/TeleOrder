be9bcf8f8cbe08d447bfc04e7b7d1cfaf3b2e0bd
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-08-04 13:24:49 +0800

    Add admin navigation drawer to Main layout
    Introduces a navigation drawer for admin users in the Main.vue layout, displaying user info and navigation links for forms, records, and users. Also adds a logout button to the app bar for improved user experience.

--- resources/js/Layout/Main.vue
+++ resources/js/Layout/Main.vue
@@ -1,7 +1,38 @@
+<script setup>
+import { usePage, Link } from "@inertiajs/vue3"
+
+const page = usePage()
+
+</script>
 <template>
     <v-layout>
+        <v-navigation-drawer
+            v-if="$page.props.user.can.admin"
+            expand-on-hover
+            permanent
+            rail
+        >
+            <v-list>
+                <v-list-item
+                    prepend-icon="mdi-account"
+                    :subtitle="$page.props.user.email"
+                    :title="$page.props.user.name"
+                ></v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-receipt-text-edit-outline" title="Form" @click="$inertia.get('/form')"></v-list-item>
+                <v-list-item prepend-icon="mdi-list-box-outline" title="Records" @click="$inertia.get('/records')"></v-list-item>
+                <v-list-item prepend-icon="mdi-account-multiple" title="Users" @click="$inertia.get('/users')"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
         <v-app-bar :elevation="2" color="orange">
             <v-app-bar-title>Application</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <v-btn href="/logout">Logout</v-btn>
         </v-app-bar>
         <v-main>
             <slot />
