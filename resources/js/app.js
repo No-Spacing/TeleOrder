18417b8abd50d7c84b7f465ce064e358bdb46124
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-08-08 13:46:08 +0800

    Remove unused Vuetify imports in app.js
    Eliminated unnecessary imports of Vuetify components and directives to clean up the code and reduce potential bundle size.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,6 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 // Register Vuetify as plugin
 const vuetify = createVuetify()
